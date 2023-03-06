7da0282686c6ce55ef77e1a132e44438d367abcf
Maksim Suvorov <suvormaks@yandex.ru>
2023-03-06 19:15:57 +0300

    MVP searchEngine

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,43 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private Map<String, List<PageEntry>> resultMap = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        String[] allFiles = pdfsDir.list();
+        for (String file : allFiles) {
+            var doc = new PdfDocument(new PdfReader(pdfsDir.getPath() + "/" + file));
+            for (int i = 1; i <= doc.getNumberOfPages(); i++) {
+                Map<String, Integer> tempMapByPage = new HashMap<>();
+                var page = doc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.toLowerCase().split(("\\P{IsAlphabetic}+"));
+                for (String word : words) {
+                    tempMapByPage.put(word, (tempMapByPage.getOrDefault(word, 0) + 1));
+                }
+                for (Map.Entry<String, Integer> entry : tempMapByPage.entrySet()) {
+                    if (resultMap.containsKey(entry.getKey())) {
+                        List<PageEntry> temp = resultMap.get(entry.getKey());
+                        temp.add(new PageEntry(file, i, entry.getValue()));
+
+                        resultMap.replace(entry.getKey(), temp);
+                    } else {
+                        resultMap.put(entry.getKey(), new ArrayList<>
+                                (Arrays.asList(new PageEntry(file, i, entry.getValue()))));
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        return resultMap.get(word.toLowerCase());
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,34 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        if (this.count > o.count) return 1;
+        else if (this.count < o.count) return -1;
+        else return 0;
+    }
+
+
+    @Override
+    public String toString() {
+        return pdfName + " - " + page + " - " + count + "\n";
+    }
 }
