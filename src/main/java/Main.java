07affb620131a6ca102ca05aaac9bfa9c107cc13
Maksim Suvorov <suvormaks@yandex.ru>
2023-03-09 18:25:49 +0300

    fix port in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.util.Arrays;
 
 public class Main {
-    private static final int PORT = 8080;
+    private static final int PORT = 8989;
     private static final File dirForSearch = new File("pdfs/");
 
     public static void main(String[] args) throws Exception {
